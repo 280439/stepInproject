3f38b4f3fdf5455ad460404a0d7eca2b93390080
280439 <82118144+280439@users.noreply.github.com>
2021-04-16 02:40:12 +0530

    Add files via upload

---
+++ 3_Implementation/test/resistor_test.c
@@ -0,0 +1,32 @@
+#include "fun.h"
+#include "unity.h"
+#define PROJECT_NAME "fun"
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+void test_search(void);
+void test_compare(void);
+
+void test_search(void){
+    TEST_ASSERT_EQUAL(0,searchValue("BLACK"));
+}
+
+void test_compare(void){
+    TEST_ASERT_EQUAL(1,compareString("BLACK","BLACK"));
+    
+}
+
+int main(void)
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_search);
+  RUN_TEST(test_compare);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
