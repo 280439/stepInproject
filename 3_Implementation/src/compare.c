4f91bf76ce940e18609746b2051291789c59000e
280439 <82118144+280439@users.noreply.github.com>
2021-04-16 10:13:01 +0530

    Add files via upload

--- 3_Implementation/src/compare.c
+++ 3_Implementation/src/compare.c
@@ -1,6 +1,22 @@
+/**
+ * @file compare.c
+ * @author Nivesh (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include "fun.h"
 #include "string.h"
-
+/**
+ * @brief 
+ * 
+ * @param string1 
+ * @param string2 
+ * @return int 
+ */
 int compareString (char *string1, char *string2)
 {
     if (!string1 && !string2) return 0;
@@ -10,4 +26,5 @@ int compareString (char *string1, char *string2)
     for (; *string1 && *string2 && *string1 == *string2; string1++, string2++) {}
 
     return *string1 - *string2;
+    
 }
